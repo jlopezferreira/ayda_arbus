4edea49cac85cbe137fe869c954c80fa2430c6cc
Javier Lopez Ferreira <javierlopez987@gmail.com>
2023-05-23 11:20:55 -0300

    implementacion inicial de arbin

--- Arbin.cpp
+++ Arbin.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include "Arbin.h"
 
 template <typename T>
@@ -6,3 +7,91 @@ Arbin<T>::Arbin()
 {
     raiz = NULL;
 }
+
+template <typename T>
+Arbin<T>::~Arbin()
+{
+    vaciar();
+}
+
+template <typename T>
+void Arbin<T>::crearArbin(T elemento)
+{
+    switch (crear(elemento)) {
+        case 1: raiz->hijoIzq->crearArbin(elemento); break;
+        case 2: raiz->hijoDer->crearArbin(elemento); break;
+        default: break;
+    }
+}
+
+template <typename T>
+bool Arbin<T>::esVacio()
+{
+    return raiz == NULL;
+}
+
+// pre: not esVacio
+template <typename T>
+T Arbin<T>::raizArbin()
+{
+    if (raiz != NULL)
+        return raiz->elemento;
+}
+
+// pre: not esVacio
+template <typename T>
+Arbin<T> * Arbin<T>::subIzquierdo()
+{
+    assert(raiz == NULL);
+    if (raiz != NULL)
+        raiz->hijoIzq = new Arbin;
+    return raiz->hijoIzq;
+}
+
+// pre: not esVacio
+template <typename T>
+Arbin<T> * Arbin<T>::subDerecho()
+{
+    assert(raiz == NULL);
+    return raiz->hijoDer;
+}
+
+template <typename T>
+bool Arbin<T>::pertenece(T elemento)
+{
+    if (raiz != NULL) {
+        if (raiz->elemento == elemento)
+            return true;
+        if(subIzquierdo()->pertenece(elemento))
+            return true;
+        if(subDerecho()->pertenece(elemento))
+            return true;
+    }
+    return false;
+}
+
+template <typename T>
+void Arbin<T>::vaciar()
+{
+    // TO-DO
+    raiz = NULL;
+}
+
+template <typename T>
+int Arbin<T>::crear(T elemento)
+{
+    if (raiz == NULL) {
+        raiz = new Nodo;
+        raiz->elemento = elemento;
+        return 0;
+    } else if (raiz->hijoIzq == NULL) {
+        raiz->hijoIzq = new Arbin;
+        return 1;
+    } else if (raiz->hijoDer == NULL) {
+        raiz->hijoDer = new Arbin;
+        return 2;
+    }
+}
+
+template class Arbin<int>;
+template class Arbin<float>;
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include "Arbin.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Arbin<int> arbin;
+    int num;
+    cout << "Ingrese cantidad de numeros: " << endl;
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+        arbin.crearArbin(i);
+
+    cout << "Ingrese numero a buscar: " << endl;
+    cin >> num;
+    if (arbin.pertenece(num))
+        cout << "Pertenece el numero " << num << endl;
+    else
+        cout << "No pertenece el numero " << num << endl;
     return 0;
 }
